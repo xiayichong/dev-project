0e647fd5e62ff91cab28663ef5c8bc8601b43e6c
“xyc” <xiayichong@foxmail.com>
2018-03-26 10:16:35 +0800

    initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Hello {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
---
+++ untitled/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+
+        test();
+    }
+
+    public static void test(){
+        System.out.println("hello");
+    }
+}
